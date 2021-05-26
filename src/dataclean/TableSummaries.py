997955af07c0b2f8e73f356cd4efda67b3d6461b
Ziming Yang <zyang@bnl.gov>
2021-05-25 20:22:03 -0400

    add stats methods using on pandas data framework

--- src/dataclean/TableSummaries.py
+++ src/dataclean/TableSummaries.py
@@ -29,3 +29,70 @@ def shapiro_columns(df, progress = False):
         print("done shapiro, shape")
         print(shapiro_out_df.shape)
     shapiro_out_df
+
+
+def row_sum(df):
+    """
+    This function takes a pandas data framework and calculates each row's summation, only for numerical
+    columns
+    Args:
+        df: a pandas data framework
+
+    Returns:
+        new : new pandas data framework containing the non-numerical columns and a new "sum" column for row sum
+    """
+    # select columns that are not numerical
+    res = df.select_dtypes(include=[object, bool])
+    res = res.assign(sum=df.sum(axis=1))
+    return res
+
+
+def row_non_zero_count(df):
+    """
+    This function counts non-zero numerical values for each row
+    Args:
+        df: a pandas data framework
+
+    Returns:
+        res : new pandas data framework containing the non-numerical columns and a new "cnt" column for row non-zero
+        values count
+    """
+    # select columns that are not numerical
+    res = df.select_dtypes(include=[object, bool])
+    # add a new column to res
+    res = res.assign(cnt=df.select_dtypes(include=np.number).astype(bool).sum(axis=1))
+    return res
+
+
+def col_sum(df):
+    """
+    This function
+    Args:
+        df: a pandas data framework
+
+    Returns:
+        res: new pandas data framework containing 2 columns. First column - 'column name' is numerical columns' names,
+        second column - 'sum' is the sum of that column.
+    """
+    # select df numerical column names and convert from series to frame
+    res = df.select_dtypes(include=np.number).columns.to_frame(name="column name")
+    # sum values column-wise and add them to be the second column
+    res['sum'] = df.select_dtypes(include=np.number).sum(axis=0)
+    return res
+
+
+def col_non_zero_count(df):
+    """
+    This function counts non-zero numerical values for each column
+    Args:
+        df: a pandas data framework
+
+    Returns:
+        res : new pandas data framework containing 2 columns. First column - 'column name' is numerical columns' names,
+        second column - 'cnt' is the count of non-zero values in that column.
+    """
+    # select df numerical column names and convert from series to frame
+    res = df.select_dtypes(include=np.number).columns.to_frame(name="column name")
+    # count non-zero values in column and add them to second column
+    res['cnt'] = df.select_dtypes(include=np.number).astype(bool).sum(axis=0).to_frame(name="column name")
+    return res
